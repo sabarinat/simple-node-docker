575bfe24cb6447134135c59f36d06b00a4161234
Sami-211 <saminathan_S@outllook.com>
2025-12-01 21:13:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Sabari! Your Node.js app is running 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
